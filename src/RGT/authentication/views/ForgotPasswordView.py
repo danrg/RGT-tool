d2512b986e49bc41eedd92d2669466ad486e76c0
Jos van der Til <jvandertil@gmail.com>
2012-06-01 14:33:35 +0200

    Refactored ForgotPasswordView
    And added basic unit test

--- src/RGT/authentication/views/ForgotPasswordView.py
+++ src/RGT/authentication/views/ForgotPasswordView.py
@@ -4,8 +4,9 @@ from django.views.generic.edit import FormView
 from RGT.authentication.forms.ForgotPasswordForm import ForgotPasswordForm
 from RGT import settings
 from RGT.authentication.models import PassRecoverCode
-from RGT.gridMng import utility
-from RGT.utils import sendMail
+from RGT.authentication.EmailService import EmailService
+from RGT.gridMng import utility #for randomStringGenerator.....
+
 
 class ForgotPasswordView(FormView):
     template_name = 'authentication/forgotPass.html'
@@ -22,25 +23,18 @@ class ForgotPasswordView(FormView):
 
         return ForgotPasswordForm()
 
-    def form_invalid(self, form):
-        return super(ForgotPasswordView, self).form_invalid(form)
-
     def form_valid(self, form):
         email = form.cleaned_data['email']
         user = User.objects.get(email=email)
-        # send the email for the new password request
-        subject = 'New password request for RGT!'
-        toMail = form.cleaned_data['email']
-        htmlContentTemplate = 'authentication/forgotPassEmail.html'
-        linkInitialPart = settings.HOST_NAME+'/accounts/recover/'
         generatedCode = utility.randomStringGenerator(14)
-        textContent = 'Click this link to change your password.'
-        fromMail = settings.EMAIL_HOST_USER
-        if sendMail(subject, toMail, fromMail, htmlContentTemplate, linkInitialPart,
-            generatedCode, user, textContent):
-            code = PassRecoverCode()
-            code.email = toMail
-            code.linkCode = generatedCode
+
+        code = PassRecoverCode()
+        code.email = email
+        code.linkCode = generatedCode
+
+        emailService = EmailService()
+
+        if emailService.sendForgotPasswordEmail(user, code):
             code.save()
             return self.render_to_response(self.get_context_data(form=form, checkEmail = True))
         else:
