57057442bb8b3170bb9e263d2be8489c932b42b8
Jos van der Til <jvandertil@gmail.com>
2012-06-01 15:57:08 +0200

    More tests

--- src/RGT/authentication/tests/EmailServiceTest.py
+++ src/RGT/authentication/tests/EmailServiceTest.py
@@ -10,6 +10,14 @@ class EmailServiceTest(TestCase):
         result = emailService.sendEmail(email)
 
         self.assertTrue(result)
-
         email.send.assert_called_with()
         pass
+
+    def test_sendEmail_returns_False_if_send_raises_an_Exception(self):
+        emailService = EmailService()
+        email = Mock()
+        email.send.side_effect = Exception('Some generic exception')
+
+        result = emailService.sendEmail(email)
+
+        self.assertFalse(result)
