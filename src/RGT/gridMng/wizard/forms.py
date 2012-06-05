ffa574bef20da677230242ac77c48612d164e88a
Jos van der Til <jvandertil@gmail.com>
2012-06-05 16:42:16 +0200

    Working on dynamic form

--- src/RGT/gridMng/wizard/forms.py
+++ src/RGT/gridMng/wizard/forms.py
@@ -1,10 +1,22 @@
 from django import forms
+from django.forms.fields import CharField
 
 class GeneralsForm(forms.Form):
     grid_name = forms.CharField()
     description = forms.CharField(widget=forms.Textarea, required=False)
     
 class AlternativesForm(forms.Form):
+
+    def __init__(self, *args, **kwargs):
+        super(AlternativesForm, self).__init__(*args, **kwargs)
+        if len(self.data) > 0:
+            for x in range(int(self.data['numAlternatives'])):
+                alternativeName = 'alternative%d' % (x+1);
+                self.fields[alternativeName] = CharField()
+                print self.data[alternativeName]
+            pass
+
+
     alternative = forms.CharField()
     
 class ConcernsForm(forms.Form):
