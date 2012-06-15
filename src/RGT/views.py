92bfe6db0d37c69cca816c56879595af82696238
Jos van der Til <jvandertil@gmail.com>
2012-06-15 14:00:18 +0200

    Secured help view function

--- src/RGT/views.py
+++ src/RGT/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic.simple import redirect_to
@@ -5,18 +6,13 @@ from HelpMessages import HELP_MESSAGES
 #import logging
 from RGT.gridMng.utility import createXmlSuccessResponse, createXmlErrorResponse
 
+@login_required
 def home(request):
-    if request.user.is_authenticated():
-        # Get an instance of the logger
-        #logger = logging.getLogger('django')
-        # Log a message at a debug level
-        #logger.warning('user is on home page')
-        profile = request.user.get_profile()
-        return render(request, 'home.html',
-                      {'firstName': request.user.first_name, 'verifiedEmail': profile.verifiedEmail})
+    profile = request.user.get_profile()
+    return render(request, 'home.html', {'firstName': request.user.first_name, 'verifiedEmail': profile.verifiedEmail})
 
-    return redirect_to(request, '/auth/login/', permanent=False)
 
+@login_required
 def help(request, helpMessageId = ''):
     if(helpMessageId in HELP_MESSAGES):
         return HttpResponse(createXmlSuccessResponse(HELP_MESSAGES[helpMessageId]), content_type='application/xml')
