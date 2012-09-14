4c74529a9cb7bb5be33dd0d484e214227fe59df5
punitharam <punitharam@gmail.com>
2012-09-14 07:42:15 +0300

    Update src/RGT/authentication/registration/views.py
    Fix to make email verification functional irrespective of whether user is logged in or not logged in

--- src/RGT/authentication/registration/views.py
+++ src/RGT/authentication/registration/views.py
@@ -1,28 +1,21 @@
 from django.views.generic.simple import redirect_to
 from RGT.userProfile.models import UserProfile
 from django.contrib.auth.models import User
-from django.utils.functional import SimpleLazyObject
 
 def verify(request, verifyEmailCode=''):
-    if not request.user.is_authenticated():
-        return redirect_to(request, '/auth/login/')
-        
     if request.method == 'GET':
-        if not request.user.is_superuser:
-            try:
-                profile = UserProfile.objects.get(user=request.user)
-                if profile.verifyEmailCode == verifyEmailCode:
-                    if not profile.verifiedEmail:
-                        profile.verifiedEmail = True
-                        profile.save()
-                        user = User.objects.get(id=request.user.id)
-                        user.is_active = True
-                        user.save()
-                else:
-                    # it is not the same user as the one logged in
-                    pass
-            except UserProfile.DoesNotExist:
-                # a profile with this user does not exist
-                pass
-        
-        return redirect_to(request, '/home/')
+        Code = verifyEmailCode
+        profile = UserProfile.objects.get(verifyEmailCode=Code)
+        useremail = profile.user
+        if not profile.verifiedEmail:
+            profile.verifiedEmail = True
+            profile.save()
+            user = User.objects.get(email=useremail)
+            user.is_active = True
+            user.save()
+        else:
+        # profile already verified
+            pass
+
+    return redirect_to(request, '/auth/login/')
+
