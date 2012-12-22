9e40e22a9652a14ca7cbb09e84d41deb76860e17
p722 <grayson82@hotmail.com>
2012-12-22 12:42:31 -0200

    Changed: changed the setting file to point to the /static folder as it was renamed from /staticFolder to /static in a previous commit.

--- src/RGT/settings.py
+++ src/RGT/settings.py
@@ -74,7 +74,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    projectPath + '/staticFiles',
+    projectPath + '/static',
 )
 
 # List of finder classes that know how to find static files in
