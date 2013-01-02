5b400d4e94a9e11c1d64636fb3567abb1cb9cae8
punitharam <punitharam@gmail.com>
2013-01-02 15:04:12 +0100

    Update static/js/wizard/wizard-step_2.js

--- static/js/wizard/wizard-step_2.js
+++ static/js/wizard/wizard-step_2.js
@@ -11,7 +11,10 @@ $(document).ready(function() {
 	    var divId = 'input'+newNum;
 	    var inputName = '1-alternative-'+newNum;
 	    var inputId = 'id_1-alternative-'+newNum;
-	    var newElem	= '<div id="'+divId+'" class="cloned"><input type="text" name="'+inputName+'" id="'+inputId+'" tabindex="'+newNum+'" size="30" /></div>'
+        //create the new button
+        var buttonId = 'btn-del-'+newNum;
+        var removefun = 'removeAlt('+newNum+')';
+        var newElem	= '<div id="'+divId+'" class="cloned"><input type="text" name="'+inputName+'" id="'+inputId+'" tabindex="'+newNum+'" size="30" /> <input type="button" id="'+buttonId+'" value="X" onclick="'+removefun+'" /></div>'
 	    // insert the new element after the last input field
 	    $('#input' + num).after(newElem);
 	    // focus on the new element
@@ -26,3 +29,33 @@ $(document).ready(function() {
 		$('#id_1-alternative-1').focus();
 	}
 });
+function removeAlt(buttonNumber) {
+    // how many cloned input fields there are currently
+    var num = $('.cloned').length;
+    // the numeric ID after the textbox is deleted
+    var newNum = new Number(num - 1);
+    var delElement = $(this).find('#input' + buttonNumber);
+    // delete element
+    if (num > 2) {
+        $('#input' + buttonNumber).remove();
+        // update count of alternatives
+        $('#num-alternatives').val(newNum);
+        var i = new Number(buttonNumber + 1);
+        var j = 0
+        for (i; i <= num; i++) {
+            j = i - 1;
+            $('#input' + i).attr('id', 'input' + j);
+            $('#id_1-alternative-' + i).attr('name', '1-alternative-' + j);
+            $('#id_1-alternative-' + i).attr('tabindex', j);
+            $('#id_1-alternative-' + i).attr('id', 'id_1-alternative-' + j);
+            $('#btn-del-' + i).attr('onclick', 'removeAlt(' + j + ')');
+            $('#btn-del-' + i).attr('id', 'btn-del-' + j);
+        }
+        //update tabindexes
+        $('#btn-add').attr('tabindex', newNum);
+        $('#btn-submit').attr('tabindex', newNum + 1);
+        $('#btn-prev-step').attr('tabindex', newNum + 2);
+    } else {
+        showMessageInDialogBox('Atleast more than 2 alternatives should exist to perform delete.');
+    }
+}
